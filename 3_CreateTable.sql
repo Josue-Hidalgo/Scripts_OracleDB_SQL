c05f109d306916651317bbeff2eda34d49b08a23
JosueSantiago25 <josuehidalgos2511@gmail.com>
2025-10-05 19:41:21 -0600

    CreateTable

--- 3_CreateTable.sql
+++ 3_CreateTable.sql
@@ -1,23 +1,23 @@
--- 0. Conexion al esquema GE
+-- 0. Conexión al esquema GE
 CONNECT GE/GE@OracleDB;
 
 -- 1. Crear tabla
 CREATE TABLE NombreTabla (
-    -- 1.1 CONSTRAINT para atributos obligatorios
+    -- 1.* Se le agregan los CONSTRAINT 
     IdNombreTabla NUMBER CONSTRAINT IdNombreTabla_nn NOT NULL,
     IdNombreTablaRelacionada NUMBER,
     PrimerAtributo VARCHAR2(50),
     -- 2. Crear PKs
     CONSTRAINT PK_NombreTabla PRIMARY KEY (IdNombreTabla)
-        USING INDEX TABLESPACE GE_INDEX;
-    -- 3. Crear PKs
+        USING INDEX TABLESPACE GE_INDEX,
+    -- 3. Crear FKs (CORREGIDO: era "PKs" pero es FOREIGN KEY)
     CONSTRAINT FK_NombreTablaRelacionada FOREIGN KEY (IdNombreTablaRelacionada)
-        REFERENCES TABLESPACE GE_INDEX;
+        REFERENCES OtraTabla(IdOtraTabla) -- FALTABA: tabla y columna de referencia
 ) TABLESPACE GE_DATA;
 
 -- 4. Create Sequences
-CREATE SEQUENCE TypeIdentification
-START WITH 0
+CREATE SEQUENCE SEQ_NombreTabla
+START WITH 1
 INCREMENT BY 1
 MINVALUE 0
 NOCACHE
